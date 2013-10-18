16e53130c31c7d0e9135c7573b7049d9ad103930
dericed <dave@dericed.com>
2013-10-17 23:50:05 -0400

    analyzeplay: require ffplay, don't specifically need to bavc ffmpeg branch

--- analyzeplay.rb
+++ analyzeplay.rb
@@ -4,7 +4,7 @@ class Analyzeplay < Formula
   homepage 'https://github.com/bavc/analyzeplay'
   head 'git://github.com/bavc/analyzeplay.git'
 
-  depends_on 'bavc/qctools/ffmpeg'
+  depends_on 'ffmpeg' => 'with-ffplay'
 
   def install
     bin.install "analyzeplay"
