ae5746c00ed78927e67c6aeb8d0d26c3dbfcdb37
Dave Rice <dericed@yahoo.com>
2014-01-19 23:45:17 -0500

    removing broken requirement

--- analyzeplay.rb
+++ analyzeplay.rb
@@ -4,7 +4,7 @@ class Analyzeplay < Formula
   homepage 'https://github.com/bavc/analyzeplay'
   head 'git://github.com/bavc/analyzeplay.git'
 
-  depends_on 'ffmpeg' => 'with-ffplay'
+  depends_on 'ffmpeg'
 
   def install
     bin.install "analyzeplay"
