4881cbd76097324f1b4269d5a70b2d3de7849d2d
Dave Rice <dave@dericed.com>
2016-01-15 13:48:48 -0500

    v0.2

--- videomachine.rb
+++ videomachine.rb
@@ -2,7 +2,9 @@ require 'formula'
 
 class Videomachine < Formula
   homepage 'https://github.com/bavc/videomachine'
+  url 'https://github.com/bavc/videomachine/archive/v0.2.zip'
   head 'git://github.com/bavc/videomachine.git'
+  sha256 '6e2f5967cddf507318947be54b02ecb5fae02f1f5387ef314fbeef498b0524b2'
 
   def install
     bin.install "barsandtoner"
